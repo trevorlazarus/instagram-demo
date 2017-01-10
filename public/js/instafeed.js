13d36ee3911a7e5d280fe1c098f23600fd3947c7
Trevor Lazarus <trevlazarus@gmail.com>
2017-01-10 16:54:32 +0100

    Update instafeed.js

--- public/js/instafeed.js
+++ public/js/instafeed.js
@@ -1,12 +1,9 @@
 (function() {
-  //Place your own Instagram client_id below. Go to https://instagram.com/developer/clients/manage/ and register yo
-ur app to get a client ID
+  //Place your own Instagram client_id below. Go to https://instagram.com/developer/clients/manage/ and register your app to get a client ID
    var client_id = '14f7d626a32f496281a194bb1032d58b';
-  //To get your user ID go to http://jelled.com/instagram/lookup-user-id and enter your Instagram user name to get 
-your user ID
+  //To get your user ID go to http://jelled.com/instagram/lookup-user-id and enter your Instagram user name to get your user ID
   var user_id = '2287159047';
-  //https://www.instagram.com/oauth/authorize/?client_id=64a12cb0db7b41da8cd2a8736770c466&redirect_uri=http://127.0
-.0.1:8080/test&response_type=token
+  //https://www.instagram.com/oauth/authorize/?client_id=64a12cb0db7b41da8cd2a8736770c466&redirect_uri=http://127.0.0.1:8080/test&response_type=token
    var access_token = '2287159047.14f7d62.a6e3401ead5549569d2dfde3b37ada39';
   var app = angular.module('instafeed', []);
   app.factory('InstagramAPI', ['$http', function($http) {
